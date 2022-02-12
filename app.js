0d4707e66c50d1f08d39493d5aa9cd49bd680c48
Esther Olusa <oyinolusa1@gmail.com>
2022-02-13 00:40:38 +0100

    First commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const tabItem = document.querySelectorAll(".tab-item");
+const tabItemContent = document.querySelectorAll(".tab-content-item");
+
+function selectItem(e) {
+  removeBorder();
+  removeShow();
+  this.classList.add("tab-border");
+  const tabContentItem = document.querySelector(`#${this.id}-content`);
+  tabContentItem.classList.add("show");
+}
+
+function removeBorder() {
+  tabItem.forEach((item) => item.classList.remove("tab-border"));
+}
+function removeShow() {
+  tabItemContent.forEach((item) => item.classList.remove("show"));
+}
+tabItem.forEach((item) => item.addEventListener("click", selectItem));
